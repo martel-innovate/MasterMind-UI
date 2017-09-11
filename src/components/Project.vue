1c2e5ddc1437c8e1624b771752c5290dbe0d4e51
Gabriele Cerfoglio <gabriele@Gabrieles-MacBook-Pro.local>
2017-09-11 16:16:42 +0200

    Adding and removing actor roles in projects

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -28,13 +28,22 @@
         </li>
       </ul>
     </div>
-    <div class="services-list">
-      <h1>Registered Roles:</h1>
+    <div class="actors-list">
+      <h1>Registered Actors:</h1>
       <ul>
         <li v-for="actor in actors">
-          {{actor.fullname}}
+          {{actor.actor.fullname}}: {{actor.role}} <button v-on:click="removeActor(actor.roleId)">-</button>
         </li>
       </ul>
+      <button v-show="!isAddingActor" v-on:click="showAddActor">Add Actor</button>
+      <div class="actor-add" v-show="isAddingActor">
+        <form id="actor">
+          <p>
+            Full Name: <input type="text" v-model="actorName">
+            <button v-on:click="addActor">Add</button>
+          </p>
+        </form>
+      </div>
     </div>
     <hr/>
     <div class="edit-project">
@@ -63,19 +72,7 @@
       axios.get(auth.getAPIUrl() + 'v1/projects/' + this.$route.params.id + '/services', {headers: {'Authorization': auth.getAuthHeader()}})
       .then(response => { this.services = response.data })
       .catch(error => { console.log(error) })
-      axios.get(auth.getAPIUrl() + 'v1/projects/' + this.$route.params.id + '/roles', {headers: {'Authorization': auth.getAuthHeader()}})
-      .then(response => {
-        this.roles = response.data
-        var actors = []
-        var promises = []
-        this.roles.forEach(function (element) {
-          promises.push(axios.get(auth.getAPIUrl() + 'v1/actors/' + element.actor_id, {headers: {'Authorization': auth.getAuthHeader()}})
-          .then(response => { actors.push(response.data) })
-          .catch(error => { console.log(error) }))
-        })
-        axios.all(promises).then(response => { this.actors = actors }).catch(error => { console.log(error) })
-      })
-      .catch(error => { console.log(error) })
+      this.updateActors()
     },
     data () {
       return {
@@ -83,7 +80,9 @@
         clusters: [],
         services: [],
         roles: [],
-        actors: []
+        actors: [],
+        isAddingActor: false,
+        actorName: ''
       }
     },
     methods: {
@@ -96,6 +95,82 @@
         .then(function (response) {
           console.log(response.data)
           router.push('/projects')
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+      },
+      updateActors: function (event) {
+        axios.get(auth.getAPIUrl() + 'v1/projects/' + this.$route.params.id + '/roles', {headers: {'Authorization': auth.getAuthHeader()}})
+        .then(response => {
+          this.roles = response.data
+          var actors = []
+          var promises = []
+          this.roles.forEach(function (role) {
+            promises.push(axios.get(auth.getAPIUrl() + 'v1/actors/' + role.actor_id, {headers: {'Authorization': auth.getAuthHeader()}})
+            .then(response => {
+              if (role.role_level_id === 1) {
+                actors.push({actor: response.data, role: 'Admin', roleId: role.id})
+              }
+              if (role.role_level_id === 2) {
+                actors.push({actor: response.data, role: 'User', roleId: role.id})
+              }
+            })
+            .catch(error => { console.log(error) }))
+          })
+          axios.all(promises).then(response => { this.actors = actors }).catch(error => { console.log(error) })
+        })
+        .catch(error => { console.log(error) })
+      },
+      showAddActor: function (event) {
+        this.isAddingActor = true
+      },
+      addActor: function (event) {
+        this.isAddingActor = false
+        var actorName = this.actorName
+        var projectId = this.$route.params.id
+        var updateActors = this.updateActors
+        axios.get(auth.getAPIUrl() + 'v1/actors', {headers: {'Authorization': auth.getAuthHeader()}})
+        .then(response => {
+          var actors = response.data
+          var actorId = ''
+          actors.forEach(function (actor) {
+            if (actor.fullname === actorName) {
+              actorId = actor.id
+            }
+          })
+          if (actorId !== '') {
+            axios({
+              method: 'post',
+              url: auth.getAPIUrl() + 'v1/projects/' + projectId + '/roles',
+              headers: { 'Authorization': auth.getAuthHeader() },
+              data: {
+                actor_id: actorId,
+                role_level_id: 2,
+                project_id: projectId
+              }
+            })
+            .then(function (response) {
+              console.log(response.data)
+              updateActors()
+            })
+            .catch(function (error) {
+              console.log(error)
+            })
+          }
+        })
+        .catch(error => { console.log(error) })
+      },
+      removeActor: function (roleId) {
+        var updateActors = this.updateActors
+        axios({
+          method: 'delete',
+          url: auth.getAPIUrl() + 'v1/projects/' + this.$route.params.id + '/roles/' + roleId,
+          headers: { 'Authorization': auth.getAuthHeader() }
+        })
+        .then(function (response) {
+          console.log(response.data)
+          updateActors()
         })
         .catch(function (error) {
           console.log(error)
