f15e9e753ce448a270c1a7d492d651c433a4ac86
Gabriele Cerfoglio <cerfoglg@Gabrieles-MacBook-Pro.local>
2018-06-19 10:14:36 +0200

    using different default oauth app

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,6 +6,6 @@ module.exports = merge(prodEnv, {
   MASTERMIND_API_HOST: process.env.MASTERMIND_API_HOST || '"localhost"',
   MASTERMIND_API_PORT: process.env.MASTERMIND_API_PORT || '"3000"',
   MASTERMIND_OAUTH_URI: process.env.MASTERMIND_OAUTH_URI || '"account.lab.fiware.org"',
-  MASTERMIND_OAUTH_CLIENT_ID: process.env.MASTERMIND_OAUTH_CLIENT_ID || '"f856da058c20414db0e946d234a5b9b1"',
+  MASTERMIND_OAUTH_CLIENT_ID: process.env.MASTERMIND_OAUTH_CLIENT_ID || '"cb19b84cbc654af2bdedaab30dc49f38"',
   MASTERMIND_OAUTH_REDIRECT_URI: process.env.MASTERMIND_OAUTH_REDIRECT_URI || '"http://localhost:3000/auth/login"'
 })
