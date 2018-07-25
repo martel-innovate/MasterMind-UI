c37333626bfb587389f6bff2f4c3a2381494a1dd
Gabriele Cerfoglio <cerfoglg@Gabrieles-MacBook-Pro.local>
2018-07-25 16:34:43 +0200

    Reworked roles page to include specific permissions

--- src/components/ActorList.vue
+++ src/components/ActorList.vue
@@ -25,6 +25,7 @@
         <td></td>
         <td>Name</td>
         <td>Role</td>
+        <td>Permissions</td>
       </tr>
     </thead>
     <tbody>
@@ -44,6 +45,35 @@
             </option>
             <option v-bind:value="'User'">
               User
+            </option>
+          </select>
+        </td>
+        <td>
+          <label>Clusters:</label>
+          <select v-model="actor.clusters_permissions" @change="changeActorPermissions(actor.roleId, 'clusters', $event)">
+            <option v-bind:value="'false'">
+              Read
+            </option>
+            <option v-bind:value="'true'">
+              Edit
+            </option>
+          </select>
+          <label>Services:</label>
+          <select v-model="actor.services_permissions" @change="changeActorPermissions(actor.roleId, 'services', $event)">
+            <option v-bind:value="'false'">
+              Read
+            </option>
+            <option v-bind:value="'true'">
+              Edit
+            </option>
+          </select>
+          <label>Subscriptions:</label>
+          <select v-model="actor.subscriptions_permissions" @change="changeActorPermissions(actor.roleId, 'subscriptions', $event)">
+            <option v-bind:value="'false'">
+              Read
+            </option>
+            <option v-bind:value="'true'">
+              Edit
             </option>
           </select>
         </td>
@@ -94,7 +124,8 @@
       // Update actors being listed
       updateActors: function (event) {
         // Get actor roles
-        axios.get(auth.getAPIUrl() + 'v1/projects/' + this.$route.params.id + '/roles', {headers: {'Authorization': auth.getAuthHeader()}})
+        var projectId = this.$route.params.id
+        axios.get(auth.getAPIUrl() + 'v1/projects/' + projectId + '/roles', {headers: {'Authorization': auth.getAuthHeader()}})
         .then(response => {
           this.roles = response.data
           var actors = []
@@ -102,19 +133,30 @@
           // Get role levels of the actors, save them in data
           this.roles.forEach(function (role) {
             // Push promise to a list
-            promises.push(axios.get(auth.getAPIUrl() + 'v1/actors/' + role.actor_id, {headers: {'Authorization': auth.getAuthHeader()}})
+            promises.push(axios.get(auth.getAPIUrl() + 'v1/projects/' + projectId + '/getactorbyrole?role_id=' + role.id, {headers: {'Authorization': auth.getAuthHeader()}})
             .then(response => {
               if (role.role_level_id === 1) {
-                actors.push({actor: response.data, role: 'Admin', roleId: role.id})
+                actors.push({actor: response.data, role: 'Admin', roleId: role.id, clusters_permissions: role.clusters_permissions, services_permissions: role.services_permissions, subscriptions_permissions: role.subscriptions_permissions})
               }
               if (role.role_level_id === 2) {
-                actors.push({actor: response.data, role: 'User', roleId: role.id})
+                actors.push({actor: response.data, role: 'User', roleId: role.id, clusters_permissions: role.clusters_permissions, services_permissions: role.services_permissions, subscriptions_permissions: role.subscriptions_permissions})
               }
             })
             .catch(error => { console.log(error) }))
           })
           // Wait for all promises to resolve
-          axios.all(promises).then(response => { this.actors = actors }).catch(error => { console.log(error) })
+          axios.all(promises).then(response => {
+            this.actors = actors
+            console.log(actors)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+          this.actors.sort(function (a, b) {
+            if (a.role.fullname < b.role.fullname) return -1
+            if (a.role.fullname > b.role.fullname) return 1
+            return 0
+          })
         })
         .catch(error => { console.log(error) })
       },
@@ -148,6 +190,39 @@
           console.log(error)
         })
       },
+      // Change the role of an actor
+      changeActorPermissions: function (roleId, permissionKind, event) {
+        var projectId = this.$route.params.id
+        console.log(permissionKind)
+        var permission = event.target.value
+        var data = {}
+        if (permissionKind === 'clusters') {
+          data = {
+            clusters_permissions: permission
+          }
+        } else if (permissionKind === 'services') {
+          data = {
+            services_permissions: permission
+          }
+        } else if (permissionKind === 'subscriptions') {
+          data = {
+            subscriptions_permissions: permission
+          }
+        }
+        // PUT action to the API
+        axios({
+          method: 'put',
+          url: auth.getAPIUrl() + 'v1/projects/' + projectId + '/roles/' + roleId,
+          headers: { 'Authorization': auth.getAuthHeader() },
+          data: data
+        })
+        .then(function (response) {
+          console.log(response.data)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+      },
       // Shows the form to add an actor
       showAddActor: function (event) {
         this.isAddingActor = true
@@ -156,44 +231,16 @@
       addActor: function (event) {
         var actorName = this.actorName
         var projectId = this.$route.params.id
-        var updateActors = this.updateActors
+        console.log(actorName)
         // Get actors again from API
-        axios.get(auth.getAPIUrl() + 'v1/actors', {headers: {'Authorization': auth.getAuthHeader()}})
+        axios.get(auth.getAPIUrl() + 'v1/projects/' + projectId + '/registerfullname?fullname=' + actorName, {headers: {'Authorization': auth.getAuthHeader()}})
         .then(response => {
-          // Check if actor that was entered exists by fullname
-          // TODO: Check by email too
-          var actors = response.data
-          var actorId = ''
-          actors.forEach(function (actor) {
-            if (actor.fullname === actorName) {
-              actorId = actor.id
-            }
-          })
-          // If actor exists, add it as a user role
-          if (actorId !== '') {
-            this.isAddingActor = false
-            axios({
-              method: 'post',
-              url: auth.getAPIUrl() + 'v1/projects/' + projectId + '/roles',
-              headers: { 'Authorization': auth.getAuthHeader() },
-              data: {
-                actor_id: actorId,
-                role_level_id: 2,
-                project_id: projectId
-              }
-            })
-            .then(function (response) {
-              console.log(response.data)
-              updateActors()
-            })
-            .catch(function (error) {
-              console.log(error)
-            })
-          } else {
-            this.actorDoesNotExistError = true
-          }
+          location.reload()
         })
-        .catch(error => { console.log(error) })
+        .catch(error => {
+          console.log(error)
+          this.actorDoesNotExistError = true
+        })
       },
       // Remove actor from project (delete its role within it)
       removeActor: function (roleId) {
