d7cfc1a307346671eb8f9fffe2880a84c10ed83e
Gabriele Cerfoglio <cerfoglg@Gabrieles-MacBook-Pro.local>
2018-08-24 16:41:34 +0200

    Fixes to how configuraton variables are passed and set before building the node app

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,9 +3,8 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  MASTERMIND_API_HOST: process.env.MASTERMIND_API_HOST || '"localhost"',
-  MASTERMIND_API_PORT: process.env.MASTERMIND_API_PORT || '"3000"',
-  MASTERMIND_OAUTH_URI: process.env.MASTERMIND_OAUTH_URI || '"account.lab.fiware.org"',
-  MASTERMIND_OAUTH_CLIENT_ID: process.env.MASTERMIND_OAUTH_CLIENT_ID || '"cb19b84cbc654af2bdedaab30dc49f38"',
-  MASTERMIND_OAUTH_REDIRECT_URI: process.env.MASTERMIND_OAUTH_REDIRECT_URI || '"http://localhost:3000/auth/login"'
+  MASTERMIND_API_HOST: JSON.stringify(process.env.MASTERMIND_API_HOST),
+  MASTERMIND_API_PORT: JSON.stringify(process.env.MASTERMIND_API_PORT),
+  MASTERMIND_OAUTH_URI: JSON.stringify(process.env.MASTERMIND_OAUTH_URI),
+  MASTERMIND_OAUTH_CLIENT_ID: JSON.stringify(process.env.MASTERMIND_OAUTH_CLIENT_ID)
 })
